4bd9436d28b344e5a947a78ef93d77a4ce324e1b
Daniel Fuchs <djfxtd@mst.edu>
2018-12-02 01:04:00 -0600

    Standardized restaurant profile.

--- data_synthesis.py
+++ data_synthesis.py
@@ -2,15 +2,34 @@
 import pandas as pd
 from data_cleaning import *
 
+######################
+# ID LIST GENERATION #
+######################
+
+def generate_user_list():
+    t = pd.read_csv("train.csv")['userID']
+    t = pd.DataFrame(t.unique())
+    t.columns = ['userID']
+    return t
+
+def generate_restaurant_list():
+    t = pd.read_csv("train.csv")['placeID']
+    t = pd.DataFrame(t.unique())
+    t.columns = ['placeID']
+    return t
+
 #####################
 # RESTAURANT TABLES #
 #####################
 
 def synthesize_restaurant_profile():
     tables = [clean_loc_geo(), clean_loc_cuisine(), clean_loc_accepts(), clean_loc_parking(), clean_loc_hours()]
-    restaurant_profile = clean_rating('placeID')
+    restaurant_profile = pd.DataFrame(generate_restaurant_list())
     for table in tables:
-        print(table)
-        print(restaurant_profile)
-        restaurant_profile = pd.merge(restaurant_profile, table, on=['placeID'])
-    return restaurant_profile
+        restaurant_profile = pd.merge(restaurant_profile, table, how='left', on=['placeID'])
+    return standardize_restaurant_profile(restaurant_profile)
+
+def standardize_restaurant_profile(table):
+    for column in table.columns:
+        table[column].fillna(table[column].mode()[0], inplace=True)
+    return table
